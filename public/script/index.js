1893ec10a4365b866ff30a3d8f4b1d1a7203b87a
hanif-page <amrhanif15@gmail.com>
2022-04-02 15:23:37 +0700

    edit copyright year, make it always display the current year right now (generated from javascript)

--- public/script/index.js
+++ public/script/index.js
@@ -2,3 +2,8 @@ const hamburgerOpen = () => {
     document.querySelector(".hamburger").classList.toggle('openNav')
     document.querySelector(".mobileNav").classList.toggle('openNav')
 }
+
+// Copyright year
+const copyrightYear = document.querySelector(".copyright .yyyy")
+const currentDate = new Date()
+copyrightYear.innerHTML = currentDate.getFullYear()
